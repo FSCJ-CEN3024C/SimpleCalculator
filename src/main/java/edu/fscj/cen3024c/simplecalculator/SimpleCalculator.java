2d2b0eadea6569113693fa1117fed0946c8917a0
Anthony Greene <97619586+1AnthonyGreene@users.noreply.github.com>
2024-08-31 14:19:50 -0400

    added SimpleCalculator code

---
+++ src/main/java/edu/fscj/cen3024c/simplecalculator/SimpleCalculator.java
@@ -0,0 +1,27 @@
+//SimpleCalculator.java
+//Anthony Greene
+//8/31/24
+
+
+
+package edu.fscj.cen3024c.simplecalculator;
+
+public class SimpleCalculator {
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static int subtract(int a, int b){
+        return a - b;
+    }
+
+    public static int divide(int a, int b){
+        return a / b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+}
+
